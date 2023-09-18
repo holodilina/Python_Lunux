b0693104f9105de3fabce91747c85f6361efe74b
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-18 19:46:03 +0300

    Create task2-stdinout.py

---
+++ task2-stdinout.py
@@ -0,0 +1,27 @@
+import subprocess
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=suprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.retruncode == 0:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+
+    # test1
+    if cheackout("cd /home/nata/tst; 7z a ../out/arx2", "Everything is OK"):
+        print("test1 SUCCESS")
+    else
+        print("test1 FAIL")
+
+    # test2
+    if checkout("cd /home/nata/out; 7z e arx2.7z -o/home/nata/folder1 -y", "Everything is OK"):  
+        print("test2 SUCCESS")
+    else
+        print("test2 FAIL")
+
+# test3
+    if checkout("cd /home/nata/out; 7z e arx2.7z -o/home/nata/folder1 -y", "Everything is OK"):  
+        print("test3 SUCCESS")
+    else
+        print("test3 FAIL")
