15264808ecb87b083be34d64e1903a4ac0c6d277
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-18 20:26:32 +0300

    Create test3_task2pytest.py

---
+++ test3_task2pytest.py
@@ -0,0 +1,25 @@
+import subprocess
+
+import pytest
+
+
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=suprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.retruncode == 0:
+        return True
+    else:
+        return False
+
+
+def test_step1():
+    # test1
+    assert cheackout("cd /home/nata/tst; 7z a ../out/arx2", "Everything is OK"), "test1 FAIL"
+
+def test_step2():
+    # test2
+    print("hello")
+    assert checkout("cd /home/nata/out; 7z e arx2.7z -o/home/nata/folder1 -y", "Everything is OK"), "test2 FAIL"
+
+def test_step3():
+    # test3
+    assert checkout("cd /home/nata/out; 7z e arx2.7z -o/home/nata/folder1 -y", "Everything is OK"), "test3 FAIL"
