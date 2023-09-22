f0e48990e3656e4bb0c8950b99e8208f6c8965f7
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-22 22:46:38 +0300

    Create test5_example.py

---
+++ test5_example.py
@@ -0,0 +1,46 @@
+import paramiko
+
+# Функция для выполнения команды по SSH
+def run_ssh_command(hostname, username, password, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, username=username, password=password)
+    stdin, stdout, stderr = client.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    client.close()
+    return output
+
+# Шаги теста
+
+# Подключение по SSH
+hostname = 'example.com'
+username = 'your_username'
+password = 'your_password'
+
+# Шаг 1. Проверка соединения с сервером
+output = run_ssh_command(hostname, username, password, 'echo "Connection Successful"')
+print(output)
+
+# Шаг 2. Создание директории
+output = run_ssh_command(hostname, username, password, 'mkdir test_directory')
+print(output)
+
+# Шаг 3. Переход в созданную директорию
+output = run_ssh_command(hostname, username, password, 'cd test_directory')
+print(output)
+
+# Шаг 4. Создание файла внутри директории
+output = run_ssh_command(hostname, username, password, 'touch test_file.txt')
+print(output)
+
+# Шаг 5. Проверка существования файла
+output = run_ssh_command(hostname, username, password, 'ls test_file.txt')
+print(output)
+
+# Шаг 6. Удаление созданных файлов и директорий
+output = run_ssh_command(hostname, username, password, 'rm test_file.txt')
+print(output)
+
+output = run_ssh_command(hostname, username, password, 'cd .. && rm -r test_directory')
+print(output)
+
