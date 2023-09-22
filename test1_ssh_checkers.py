26f0051f6f55f74cad58ecde5b77cb450b60d13d
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-22 21:03:00 +0300

    Update test1_ssh_checkers.py

--- test1_ssh_checkers.py
+++ test1_ssh_checkers.py
@@ -5,7 +5,7 @@ def ssh_checkout(host, user, passwd, cmd, text, port=22):
     client.set_missing_host_key_policy(paramico.AutoAddPolicy())
     client.connect(hostname=host, username=user, password=passwd, port=port)
     stdin, stdout, stderr = client.exec_command(cmd)
-    exit_code = stdout.channel.send_exit_status()
+    exit_code = stdout.channel.recv_exit_status()
     out = (stdout.read() + stderr.read()).decode("utf-8")
     client.close()
     if text in out and exit_code == 0:
