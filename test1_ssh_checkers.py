2abbaa435689b6fcef13789be800ca155b0fb3fb
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-22 20:37:24 +0300

    Update test1_ssh_checkers.py

--- test1_ssh_checkers.py
+++ test1_ssh_checkers.py
@@ -13,4 +13,12 @@ def ssh_checkout(host, user, passwd, cmd, text, port=22):
     else:
        return False
 
-def upload_files(host, user, passwd,
+def upload_files(host, user, passwd, local_path, remote_path, port=22):
+    print(f"Загружаем файл {local_path} в каталог {remote_path}")
+    transport = paramico.Transport((host, port))
+    trafnsport.connect(None, username=user, password=passwd)
+    sfpt = paramico.SFTPClient.from_transport(transport)
+    sftp.put(local_path, remote_path)
+    if sftp:
+        sftp.close()
+    if transport.close()
