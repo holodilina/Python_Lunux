ab25d3028125b659857f6dd99d87c5f6ceb1b387
holodilina <112629404+holodilina@users.noreply.github.com>
2023-09-22 20:45:00 +0300

    Update test_exemple.py

--- test_exemple.py
+++ test_exemple.py
@@ -1,7 +1,8 @@
-Пример кода на языке Python, выполняющего шаги позитивных тестов по SSH:
-
 import paramiko
 
+import subprocess
+
+import pytest
 
 # Функция для выполнения команды по SSH
 def run_ssh_command(hostname, username, password, command):
@@ -14,7 +15,48 @@ def run_ssh_command(hostname, username, password, command):
     return output
 
 
-# Шаги теста
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.retruncode == 0:
+        return True
+    else:
+        return False
+
+
+def test1():
+    # test1
+    assert checkout("cd /home/nata/tst; 7z a ../out/arx2", "Everything is OK"), "test1 FAIL"
+
+def test2():
+    # test2
+    print("hello")
+    assert checkout("cd /home/nata/out; 7z e arx2.7z -o/home/nata/folder1 -y", "Everything is OK"), "test2 FAIL"
+
+def test3():
+    # test3
+    assert checkout("cd /home/nata/out; 7z t arx2.7z", "Everything is OK"), "test3 FAIL"import subprocess
+
+
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    if text in result.stdout and result.retruncode == 0:
+        return True
+    else:
+        return False
+
+
+def test1():
+    # test1
+    assert checkout("cd /home/nata/tst; 7z a ../out/arx2", "Everything is OK"), "test1 FAIL"
+
+def test2():
+    # test2
+    print("hello")
+    assert checkout("cd /home/nata/out; 7z e arx2.7z -o/home/nata/folder1 -y", "Everything is OK"), "test2 FAIL"
+
+def test3():
+    # test3
+    assert checkout("cd /home/nata/out; 7z t arx2.7z", "Everything is OK"), "test3 FAIL"
 
 # Подключение по SSH
 hostname = 'example.com'
@@ -47,5 +89,4 @@ print(output)
 
 output = run_ssh_command(hostname, username, password, 'cd .. && rm -r test_directory')
 print(output)
-Результат выполнения каждого шага будет выведен в консоль.
 
